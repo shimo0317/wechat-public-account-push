c95535ccf5e650fbc09b7012672b81f3b7d0d451
shimo0317 <132515459+shimo0317@users.noreply.github.com>
2023-05-04 08:35:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'ab67721c32519b1942c27f0ca1e9e57a',
 
-  PROVINCE: '浙江',
-  CITY: '丽水',
+  PROVINCE: '',
+  CITY: '龙泉',
 
   USERS: [
     {
@@ -23,20 +23,20 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'olZu46DoCO6KXoZll0jIjJGpkip4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '2vRCA19qxoBxiRH4nPiP_0MJ7WxoDQX2lwRP4AAus4Y',
+      useTemplateId: 'xfNnyIPkVJGZNEhzdeRE9niu2Sjfvpg2UcfTYJUtVfk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-27',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '生日', name: '你', year: '2001', date: '01-27',
+          type: '*生日', name: '你', year: '2001', date: '01-06',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '我', year: '2000', date: '03-17',
+          type: '生日', name: '你', year: '2001', date: '01-27',
         },
         {
-          type: '节日', name: '相识', year: '2023', date: '02-19',
+          type: '节日', name: '我', year: '2000', date: '03-17',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
