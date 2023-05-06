f67d85c0895a4d2948e0b82b0112860c7dc5f258
shimo0317 <132515459+shimo0317@users.noreply.github.com>
2023-05-06 21:48:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'olZu46DoCO6KXoZll0jIjJGpkip4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'eiIh_GpQlVstx1JwSzNVc9Gkmn8iLxYVkrGynL_VzYQ',
+      useTemplateId: 'EhcDaWTpCWOJvQV_3gbmKGHZq6EzyeqnulxxC9UsNQg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-27',
       festivals: [
